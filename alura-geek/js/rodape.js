5b6b8168158e1400a00937ffe5e43b1fa9814f98
Mateus Oliveira <mateus.he.oliveira@gmail.com>
2022-03-24 17:42:09 -0300

    Organizando rodape.js

--- alura-geek/js/rodape.js
+++ alura-geek/js/rodape.js
@@ -1,8 +1,21 @@
 const form = document.getElementById('novaMensagem');
+const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault();
+
+    const nome = evento.target.elements['nome'];
+    const mensagem = evento.target.elements['mensagem'];
+
+    const usuario = {
+        "nome": nome.value,
+        "mensagem": mensagem.value
+    }
+
+    usuarios.push(usuario);
+
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
     
-    console.log(evento.target.elements["nome"].value);
-    console.log(evento.target.elements["mensagem"].value);
-})
+    nome.value = "";
+    mensagem.value = "";
+});
